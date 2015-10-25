a763ca54676440f75c0b8408b6b65ec413094dad
Basyuk <kirillbasuk@gmail.com>
2015-10-25 20:40:59 +0700

    Добавлен интерфейс игрового окна и привязка кнопок к функциям

--- GameWindow.cpp
+++ GameWindow.cpp
@@ -1,15 +1,54 @@
-#include "GameWindow.h"
-#include "ui_GameWindow.h"
+#include "gamewindow.h"
+#include "ui_gamewindow.h"
 
-GameWindow::GameWindow(Game *game, QWidget *parent) :
-	QMainWindow(parent),
-	ui(new Ui::GameWindow)
+GameWindow::GameWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::GameWindow)
 {
-	ui->setupUi(this);
-	this->game = game;
+    ui->setupUi(this);
+    ui->stop_button->setDisabled(true);
+    ui->reset_button->setDisabled(true);
 }
 
 GameWindow::~GameWindow()
 {
-	delete ui;
+    delete ui;
+}
+
+void GameWindow::on_start_button_clicked()
+{
+    ui->stop_button->setEnabled(true);
+    ui->reset_button->setEnabled(true);
+    ui->start_button->setDisabled(true);
+    ui->map_button->setDisabled(true);
+    ui->death_speed->setDisabled(true);
+    ui->eat_index->setDisabled(true);
+    ui->object_index->setDisabled(true);
+    ui->snake_speed->setDisabled(true);
+    ui->select_snake->setDisabled(true);
+    ui->snake_intelligence->setDisabled(true);
+}
+
+void GameWindow::on_stop_button_clicked()
+{
+    ui->stop_button->setDisabled(true);
+    ui->reset_button->setEnabled(true);
+    ui->start_button->setEnabled(true);
+    ui->map_button->setEnabled(true);
+    ui->death_speed->setEnabled(true);
+    ui->eat_index->setEnabled(true);
+    ui->object_index->setEnabled(true);
+    ui->snake_speed->setEnabled(true);
+    ui->select_snake->setEnabled(true);
+    ui->snake_intelligence->setEnabled(true);
+}
+
+void GameWindow::on_reset_button_clicked()
+{
+    ui->reset_button->setDisabled(true);
+}
+
+void GameWindow::on_map_button_clicked()
+{
+
 }
